159804d25647f2f40cf29c39bbc0f00910a62dc3
Suraj Kumar <kumarsuraj.islampur@gmail.com>
2025-06-12 23:18:40 +0530

    added schema of trigger and action and zaprun

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,12 +1,38 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const app = express();
+
 
 // https://hooks.zapier.com/hooks/catch/1234567/2342424/
 
 // password logic
 
-app.post('/hooks/catch/:hookId/:zapId?', (req, res) => {
+app.post('/hooks/catch/:hookId/:zapId?', async (req, res) => {
     const userId = req.params.hookId;
-    const zapid = req.params.zapId;
+    const zapId = req.params.zapId;
+    const body = req.body;
+    console.log('Received hook:', { userId, zapId, body });=
+    await prisma.$transaction(async tx => {
+        const run = await prisma.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });
+        await prisma.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id,
+            }
+        })
+    })
+    // store in db a new trigger
+    await prisma.zapRun.create({
+        data: {
+            zapId: zapId
+
+        }
+    })
 })
